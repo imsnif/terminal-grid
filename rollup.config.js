8fd80675904f617c2852e04cb6cdeeb57308aea2
Aram Drevekenin <grimsniffer@gmail.com>
2016-08-24 20:36:33 +0300

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import istanbul from 'rollup-plugin-istanbul';
+
+import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
+
+let pkg = require('./package.json');
+let external = Object.keys(pkg.dependencies);
+
+export default {
+  entry: 'lib/main.js',
+  plugins: [
+    nodeResolve({
+      jsnext: true,
+      main: true
+    }),
+    commonjs({
+      include: ['node_modules/**', 'lib/**', 'addons/**', 'src/**'],
+      extensions: [ '.js' ],
+      ignoreGlobal: false,
+      sourceMap: false,
+    })
+  ],
+  external: external,
+  targets: [
+    {
+      dest: pkg['main'],
+      format: 'iife',
+      moduleName: 'terminal',
+      sourceMap: true
+    }
+  ]
+};
