83b358cd624ce0dd46786ec7d17bd1e6595487c2
Aram Drevekenin <grimsniffer@gmail.com>
2016-10-03 01:07:16 +0300

    refactor: move taskbar detection to detect-taskbars package

--- utils/screen-grid.js
+++ utils/screen-grid.js
@@ -4,77 +4,11 @@ const electron = require('electron')
 const BrowserWindow = electron.BrowserWindow
 const TerminalWindow = require('electron-terminal-window')
 const rendererVal = require('electron-renderer-value')
+const detectTaskbars = require('detect-taskbars')
 
 const Grid = require('grid')
 
 let grids = []
-
-function testWindow (bounds) {
-  const ipcMain = electron.ipcMain
-  const win = new BrowserWindow({
-    y: bounds.y,
-    x: bounds.x,
-    frame: false,
-    skipTaskbar: true
-  })
-  win.loadURL(`file://${__dirname}/index.html`)
-  win.maximize()
-  win.minimize()
-  const changedBounds = win.getContentBounds()
-  return new Promise(resolve => win.on('maximize', resolve))
-  .then(() => rendererVal(win.webContents, '{width: window.outerWidth, height: window.outerHeight}'))
-  .then(size => {
-    win.close()
-    return Object.assign({}, size, {x: changedBounds.x, y: changedBounds.y})
-  })
-  .catch(e => console.error('e:', e))
-}
-
-function findBars (bounds, workArea) {
-  if (workArea.y > bounds.y) {
-    return ({
-      id: 'topBar',
-      width: workArea.width,
-      height: workArea.y - bounds.y,
-      x: 0,
-      y: 0
-    })
-  }
-  if (bounds.y - workArea.y + bounds.height - workArea.height > 0) {
-    return ({
-      id: 'bottomBar',
-      width: workArea.width,
-      height: bounds.height - workArea.height,
-      y: workArea.height,
-      x: 0
-    })
-  }
-  if (workArea.x > bounds.x) {
-    return ({
-      id: 'leftBar',
-      width: workArea.x - bounds.x,
-      height: workArea.height,
-      x: 0,
-      y: 0
-    })
-  }
-  if (bounds.x - workArea.x + bounds.width - workArea.width > 0) {
-    return ({
-      id: 'rightBar',
-      width: bounds.x - workArea.x + bounds.width - workArea.width,
-      height: workArea.height,
-      x: workArea.width,
-      y: 0
-    })
-  }
-}
-
-function detectPanel (bounds) {
-  const screen = electron.screen
-  return testWindow(bounds)
-  .then((workArea) => findBars(bounds, workArea))
-  .catch(e => console.error(e))
-}
 
 function getGrid (winId) {
   if (winId === undefined) {
@@ -102,7 +36,7 @@ module.exports = {
     const displays = screen.getAllDisplays()
     return Promise.all(
       displays.map(display => {
-        return detectPanel(display.bounds)
+        return detectTaskbars(display.bounds)
         .then(panel => {
           const bounds = display.bounds
           const gridOffset = {x: bounds.x, y: bounds.y}
