0a0a7bef74b86f0d0d21d1566025c4e54ad88a3d
Aram Drevekenin <grimsniffer@gmail.com>
2016-11-08 21:24:20 +0200

    test: pane-grid-actions component

---
+++ test/unit/pane-grid-actions.spec.js
@@ -0,0 +1,75 @@
+const test = require('tape')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
+
+function stubPaneGridActions () {
+  return proxyquire('../../components/pane-grid-actions', {
+    '../lib/mode-at-current-window': (modes) => modes[0]
+  })
+}
+
+function stubPaneGridActionsNoMode () {
+  return proxyquire('../../components/pane-grid-actions', {
+    '../lib/mode-at-current-window': (modes) => {}
+  })
+}
+
+test('switchPaneFocus(direction): switches pane focus to direction', t => {
+  t.plan(2)
+  try {
+    const modes = [ {switchPaneFocus: sinon.spy()}, {switchPaneFocus: sinon.spy()} ]
+    const paneGridActions = stubPaneGridActions()
+    const { switchPaneFocus } = paneGridActions({modes})
+    switchPaneFocus('right')
+    t.ok(
+      modes[0].switchPaneFocus.calledWith('right'),
+      'switchPaneFocus method of relevant mode called with given direction'
+    )
+    t.ok(modes[1].switchPaneFocus.notCalled, 'switchPaneFocus of other mode not called')
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('switchPaneFocus(direction): does not switch pane focus when no win is focused', t => {
+  t.plan(1)
+  try {
+    const modes = [ {switchPaneFocus: sinon.spy()}, {switchPaneFocus: sinon.spy()} ]
+    const paneGridActions = stubPaneGridActionsNoMode()
+    const { switchPaneFocus } = paneGridActions({modes})
+    switchPaneFocus('right')
+    t.ok(modes.every(m => m.switchPaneFocus.notCalled), 'no switchFocus method called')
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('closePane(): closes focused pane', t => {
+  t.plan(2)
+  try {
+    const modes = [ {closeCurrentPane: sinon.spy()}, {closeCurrentPane: sinon.spy()} ]
+    const paneGridActions = stubPaneGridActions()
+    const { closePane } = paneGridActions({modes})
+    closePane()
+    t.ok(
+      modes[0].closeCurrentPane.calledOnce,
+      'closePane method of relevant mode called'
+    )
+    t.ok(modes[1].closeCurrentPane.notCalled, 'closePane of other mode not called')
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('closePane(): does not close pane when no win is focused', t => {
+  t.plan(1)
+  try {
+    const modes = [ {closeCurrentPane: sinon.spy()}, {closeCurrentPane: sinon.spy()} ]
+    const paneGridActions = stubPaneGridActionsNoMode()
+    const { closePane } = paneGridActions({modes})
+    closePane()
+    t.ok(modes.every(m => m.closeCurrentPane.notCalled), 'no closePane method called')
+  } catch (e) {
+    t.fail(e)
+  }
+})
