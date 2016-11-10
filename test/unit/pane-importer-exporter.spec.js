2d00e58e6b1b40c3dea4b0f5ac1c5314fe9443f1
Aram Drevekenin <grimsniffer@gmail.com>
2016-11-11 00:52:25 +0200

    test: pane-importer-exporter - tmuxMode

--- test/unit/pane-importer-exporter.spec.js
+++ test/unit/pane-importer-exporter.spec.js
@@ -4,16 +4,12 @@ const proxyquire = require('proxyquire')
 
 function stubPaneImporterExporter () {
   return proxyquire('../../components/pane-importer-exporter', {
-//    '../lib/mode-at-current-window': (modes) => modes[0],
-//    electron: {BrowserWindow: {getFocusedWindow: () => ({id})}}
+    '../lib/utils': {
+      paneIsOnScreenEdge: (pane) => pane.onScreenEdge,
+      paneOccupiesSameSpaceAtDirection: (pane) => pane.occupiesSameSpaceAtDirection
+    }
   })
 }
-
-// function stubPaneMoverNoMode () {
-//   return proxyquire('../../components/pane-mover', {
-//     '../lib/mode-at-current-window': (modes) => {}
-//   })
-// }
 
 test('generalMode.canImport(win): returns true if gaps found in grid', t => {
   t.plan(1)
@@ -101,6 +97,159 @@ test('generalMode.export(win): expels pane from grid', t => {
     const { exportPane } = paneImporterExporter.generalMode({grid})
     exportPane('I am a paneId')
     t.ok(grid.expel.calledWith('I am a paneId'), 'expel method of grid called')
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('tmuxMode.canImport(win): returns true', t => {
+  t.plan(1)
+  try {
+    const paneImporterExporter = stubPaneImporterExporter()
+    const { canImport } = paneImporterExporter.tmuxMode()
+    t.ok(canImport('I am a window'), 'can always import pane')
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('tmuxMode.importPane(win, direction): splits largest pane horizontally when importing left', t => {
+  t.plan(1)
+  try {
+    const grid = {
+      panes: [
+        {id: 1, onScreenEdge: false},
+        {id: 2, onScreenEdge: true, occupiesSameSpaceAtDirection: false},
+        {id: 3, onScreenEdge: true, occupiesSameSpaceAtDirection: true, height: 10},
+        {id: 4, onScreenEdge: true, occupiesSameSpaceAtDirection: true, height: 20}
+      ]
+    }
+    const sGrid = {splitCurrentWindow: sinon.spy()}
+    const id = 1
+    const winOpts = 2
+    const paneImporterExporter = stubPaneImporterExporter()
+    const win = {id: 'I am a window', getBounds: () => {}}
+    const { importPane } = paneImporterExporter.tmuxMode({grid, sGrid, id, winOpts})
+    importPane(win, 'left')
+    t.ok(
+      sGrid.splitCurrentWindow.calledWith(1, win, 2, 'horizontal', 4),
+      'current window split with imported window inserted'
+    )
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('tmuxMode.importPane(win, direction): splits largest pane horizontally when importing right', t => {
+  t.plan(1)
+  try {
+    const grid = {
+      panes: [
+        {id: 1, onScreenEdge: false},
+        {id: 2, onScreenEdge: true, occupiesSameSpaceAtDirection: false},
+        {id: 3, onScreenEdge: true, occupiesSameSpaceAtDirection: true, height: 10},
+        {id: 4, onScreenEdge: true, occupiesSameSpaceAtDirection: true, height: 20}
+      ]
+    }
+    const sGrid = {splitCurrentWindow: sinon.spy()}
+    const id = 1
+    const winOpts = 2
+    const paneImporterExporter = stubPaneImporterExporter()
+    const win = {id: 'I am a window', getBounds: () => {}}
+    const { importPane } = paneImporterExporter.tmuxMode({grid, sGrid, id, winOpts})
+    importPane(win, 'right')
+    t.ok(
+      sGrid.splitCurrentWindow.calledWith(1, win, 2, 'horizontal', 4),
+      'current window split with imported window inserted'
+    )
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('tmuxMode.importPane(win, direction): splits largest pane vertically when importing up', t => {
+  t.plan(1)
+  try {
+    const grid = {
+      panes: [
+        {id: 1, onScreenEdge: false},
+        {id: 2, onScreenEdge: true, occupiesSameSpaceAtDirection: false},
+        {id: 3, onScreenEdge: true, occupiesSameSpaceAtDirection: true, width: 10},
+        {id: 4, onScreenEdge: true, occupiesSameSpaceAtDirection: true, width: 20}
+      ]
+    }
+    const sGrid = {splitCurrentWindow: sinon.spy()}
+    const id = 1
+    const winOpts = 2
+    const paneImporterExporter = stubPaneImporterExporter()
+    const win = {id: 'I am a window', getBounds: () => {}}
+    const { importPane } = paneImporterExporter.tmuxMode({grid, sGrid, id, winOpts})
+    importPane(win, 'up')
+    t.ok(
+      sGrid.splitCurrentWindow.calledWith(1, win, 2, 'vertical', 4),
+      'current window split with imported window inserted'
+    )
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('tmuxMode.importPane(win, direction): splits largest pane vertically when importing down', t => {
+  t.plan(1)
+  try {
+    const grid = {
+      panes: [
+        {id: 1, onScreenEdge: false},
+        {id: 2, onScreenEdge: true, occupiesSameSpaceAtDirection: false},
+        {id: 3, onScreenEdge: true, occupiesSameSpaceAtDirection: true, width: 10},
+        {id: 4, onScreenEdge: true, occupiesSameSpaceAtDirection: true, width: 20}
+      ]
+    }
+    const sGrid = {splitCurrentWindow: sinon.spy()}
+    const id = 1
+    const winOpts = 2
+    const paneImporterExporter = stubPaneImporterExporter()
+    const win = {id: 'I am a window', getBounds: () => {}}
+    const { importPane } = paneImporterExporter.tmuxMode({grid, sGrid, id, winOpts})
+    importPane(win, 'down')
+    t.ok(
+      sGrid.splitCurrentWindow.calledWith(1, win, 2, 'vertical', 4),
+      'current window split with imported window inserted'
+    )
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('tmuxMode.exportPane(paneId): returns pane, maxes all windows and fills gaps', t => {
+  t.plan(5)
+  try {
+    const win = 'I am a window'
+    const grid = {
+      maxAllPanes: sinon.spy(),
+      expel: (paneId) => paneId === 1 ? win : 'I am not win!',
+      findGaps: () => [{gap: 'gap1'}, {gap: 'gap2'}]
+    }
+    const sGrid = {createWindow: sinon.spy()}
+    const id = 1
+    const winOpts = {winOpts: 'I am some winOpts!'}
+    const TerminalWindow = 'I am a TerminalWindow'
+    const paneImporterExporter = stubPaneImporterExporter()
+    const { exportPane } = paneImporterExporter.tmuxMode({grid, sGrid, id, winOpts, TerminalWindow})
+    const exportedWin = exportPane(1)
+    const firstCall = sGrid.createWindow.getCall(0)
+    const secondCall = sGrid.createWindow.getCall(1)
+    t.equals(exportedWin, win, 'win returned from export method')
+    t.ok(grid.maxAllPanes.calledOnce, 'max all panes called when window exported')
+    t.ok(grid.maxAllPanes.calledBefore(sGrid.createWindow), 'max all panes called before filling gaps')
+    t.ok(
+      firstCall.calledWith(1, TerminalWindow, {gap: 'gap1', winOpts: 'I am some winOpts!'}),
+      'first gap filled'
+    )
+    t.ok(
+      secondCall.calledWith(1, TerminalWindow, {gap: 'gap2', winOpts: 'I am some winOpts!'}),
+      'second gap filled'
+    )
   } catch (e) {
     t.fail(e)
   }
