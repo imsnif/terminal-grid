11e6a4559c338cae28670a1aace30603dbbc2b71
Aram Drevekenin <grimsniffer@gmail.com>
2016-11-11 14:46:11 +0200

    test: mode-at-current-window

---
+++ test/unit/mode-at-current-window.spec.js
@@ -0,0 +1,53 @@
+const test = require('tape')
+// const sinon = require('sinon')
+const proxyquire = require('proxyquire')
+
+function stubModeAtCurrentWindow (id) {
+  return proxyquire('../../lib/mode-at-current-window', {
+    electron: {BrowserWindow: {getFocusedWindow: () => ({id})}}
+  })
+}
+
+test('modeAtCurrentWindow(modes): returns mode of screen containing focused window', t => {
+  t.plan(1)
+  try {
+    const modeAtCurrentWindow = stubModeAtCurrentWindow(3)
+    const modes = [
+      {
+        grid: {
+          panes: [ {id: 1}, {id: 2} ]
+        }
+      },
+      {
+        grid: {
+          panes: [ {id: 3}, {id: 4} ]
+        }
+      }
+    ]
+    t.equals(modeAtCurrentWindow(modes), modes[1])
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('modeAtCurrentWindow(modes): returns nothing if there is no focused window', t => {
+  t.plan(1)
+  try {
+    const modeAtCurrentWindow = stubModeAtCurrentWindow()
+    const modes = [
+      {
+        grid: {
+          panes: [ {id: 1}, {id: 2} ]
+        }
+      },
+      {
+        grid: {
+          panes: [ {id: 3}, {id: 4} ]
+        }
+      }
+    ]
+    t.equals(modeAtCurrentWindow(modes), undefined)
+  } catch (e) {
+    t.fail(e)
+  }
+})
