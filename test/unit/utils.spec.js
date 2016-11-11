85e0a6bac32bf0ed90edf7783bde744c9257cf01
Aram Drevekenin <grimsniffer@gmail.com>
2016-11-11 16:44:02 +0200

    test: pane occupies same space at direction

--- test/unit/utils.spec.js
+++ test/unit/utils.spec.js
@@ -116,244 +116,146 @@ test('paneIsOnScreenEdge(pane, grid, direction): returns false if pane is not on
   }
 })
 
-// test('movePanePrimary(direction): no-op when no focused window', t => {
-//   t.plan(1)
-//   try {
-//     const paneMover = stubPaneMoverNoMode()
-//     const modes = [ {movePaneMain: sinon.spy()}, {movePaneMain: sinon.spy()} ]
-//     const { movePanePrimary } = paneMover({modes})
-//     movePanePrimary('left')
-//     t.ok(modes.every(m => m.movePaneMain.notCalled), 'no movePaneMain method called')
-//   } catch (e) {
-//     t.fail(e)
-//   }
-// })
-//
-// test('movePanePrimary(direction): skips pane to next grid if it exists', t => {
-//   t.plan(1)
-//   try {
-//     const paneMover = stubPaneMover(9)
-//     const modes = [
-//       {
-//         id: 0,
-//         grid: {id: 0},
-//         movePaneMain: sinon.stub.throws(),
-//         canImport: sinon.spy(),
-//         importPane: sinon.spy(),
-//         exportPane: () => 'I am a pane'
-//       },
-//       {
-//         id: 1,
-//         grid: {id: 1},
-//         movePaneMain: sinon.spy(),
-//         canImport: () => true,
-//         importPane: sinon.spy(),
-//         exportPane: sinon.spy()
-//       }
-//     ]
-//     const sGrid = {
-//       adjacentGrids: [{left: {id: 1}}]
-//     }
-//     const { movePanePrimary } = paneMover({modes, sGrid})
-//     movePanePrimary('left')
-//     t.ok(modes[1].importPane.calledWith('I am a pane', 'left'), 'pane properly imported into adjacent grid')
-//   } catch (e) {
-//     t.fail(e)
-//   }
-// })
-//
-// test('movePanePrimary(direction): no op if no room for grid and no adjacent grid', t => {
-//   t.plan(2)
-//   try {
-//     const paneMover = stubPaneMover(9)
-//     const modes = [
-//       {
-//         id: 0,
-//         grid: {id: 0},
-//         movePaneMain: sinon.stub.throws(),
-//         canImport: sinon.spy(),
-//         importPane: sinon.spy(),
-//         exportPane: sinon.spy()
-//       },
-//       {
-//         id: 1,
-//         grid: {id: 1},
-//         movePaneMain: sinon.spy(),
-//         canImport: () => true,
-//         importPane: sinon.spy(),
-//         exportPane: sinon.spy()
-//       }
-//     ]
-//     const sGrid = {
-//       adjacentGrids: [{right: {id: 1}}]
-//     }
-//     const { movePanePrimary } = paneMover({modes, sGrid})
-//     movePanePrimary('left')
-//     t.ok(modes[0].exportPane.notCalled, 'pane not exported from grid')
-//     t.ok(modes[1].importPane.notCalled, 'pane not imported to non-adjacent grid')
-//   } catch (e) {
-//     t.fail(e)
-//   }
-// })
-//
-// test('movePanePrimary(direction): no op if adjacent grid cannot import pane', t => {
-//   t.plan(2)
-//   try {
-//     const paneMover = stubPaneMover(9)
-//     const modes = [
-//       {
-//         id: 0,
-//         grid: {id: 0},
-//         movePaneMain: sinon.stub.throws(),
-//         canImport: sinon.spy(),
-//         importPane: sinon.spy(),
-//         exportPane: sinon.spy()
-//       },
-//       {
-//         id: 1,
-//         grid: {id: 1},
-//         movePaneMain: sinon.spy(),
-//         canImport: () => false,
-//         importPane: sinon.spy(),
-//         exportPane: sinon.spy()
-//       }
-//     ]
-//     const sGrid = {
-//       adjacentGrids: [{left: {id: 1}}]
-//     }
-//     const { movePanePrimary } = paneMover({modes, sGrid})
-//     movePanePrimary('left')
-//     t.ok(modes[0].exportPane.notCalled, 'pane not exported from grid')
-//     t.ok(modes[1].importPane.notCalled, 'pane not imported to non-adjacent grid')
-//   } catch (e) {
-//     t.fail(e)
-//   }
-// })
-//
-// test('movePaneSecondary(direction): moves pane secondary in direction if there is room', t => {
-//   t.plan(2)
-//   try {
-//     const paneMover = stubPaneMover()
-//     const modes = [ {movePaneSecondary: sinon.spy()}, {movePaneSecondary: sinon.spy()} ]
-//     const { movePaneSecondary } = paneMover({modes})
-//     movePaneSecondary('left')
-//     t.ok(modes[0].movePaneSecondary.calledWith('left'), 'movePaneSecondary of current mode called')
-//     t.ok(modes[1].movePaneSecondary.notCalled, 'movePaneSecondary of other mode not called')
-//   } catch (e) {
-//     t.fail(e)
-//   }
-// })
-//
-// test('movePaneSecondary(direction): no-op when no focused window', t => {
-//   t.plan(1)
-//   try {
-//     const paneMover = stubPaneMoverNoMode()
-//     const modes = [ {movePaneSecondary: sinon.spy()}, {movePaneSecondary: sinon.spy()} ]
-//     const { movePaneSecondary } = paneMover({modes})
-//     movePaneSecondary('left')
-//     t.ok(modes.every(m => m.movePaneSecondary.notCalled), 'no movePaneSecondary method called')
-//   } catch (e) {
-//     t.fail(e)
-//   }
-// })
-//
-// test('movePaneSecondary(direction): skips pane to next grid if it exists', t => {
-//   t.plan(1)
-//   try {
-//     const paneMover = stubPaneMover(9)
-//     const modes = [
-//       {
-//         id: 0,
-//         grid: {id: 0},
-//         movePaneSecondary: sinon.stub.throws(),
-//         canImport: sinon.spy(),
-//         importPane: sinon.spy(),
-//         exportPane: () => 'I am a pane'
-//       },
-//       {
-//         id: 1,
-//         grid: {id: 1},
-//         movePaneSecondary: sinon.spy(),
-//         canImport: () => true,
-//         importPane: sinon.spy(),
-//         exportPane: sinon.spy()
-//       }
-//     ]
-//     const sGrid = {
-//       adjacentGrids: [{left: {id: 1}}]
-//     }
-//     const { movePaneSecondary } = paneMover({modes, sGrid})
-//     movePaneSecondary('left')
-//     t.ok(modes[1].importPane.calledWith('I am a pane', 'left'), 'pane properly imported into adjacent grid')
-//   } catch (e) {
-//     t.fail(e)
-//   }
-// })
-//
-// test('movePaneSecondary(direction): no op if no room for grid and no adjacent grid', t => {
-//   t.plan(2)
-//   try {
-//     const paneMover = stubPaneMover(9)
-//     const modes = [
-//       {
-//         id: 0,
-//         grid: {id: 0},
-//         movePaneSecondary: sinon.stub.throws(),
-//         canImport: sinon.spy(),
-//         importPane: sinon.spy(),
-//         exportPane: sinon.spy()
-//       },
-//       {
-//         id: 1,
-//         grid: {id: 1},
-//         movePaneSecondary: sinon.spy(),
-//         canImport: () => true,
-//         importPane: sinon.spy(),
-//         exportPane: sinon.spy()
-//       }
-//     ]
-//     const sGrid = {
-//       adjacentGrids: [{right: {id: 1}}]
-//     }
-//     const { movePaneSecondary } = paneMover({modes, sGrid})
-//     movePaneSecondary('left')
-//     t.ok(modes[0].exportPane.notCalled, 'pane not exported from grid')
-//     t.ok(modes[1].importPane.notCalled, 'pane not imported to non-adjacent grid')
-//   } catch (e) {
-//     t.fail(e)
-//   }
-// })
-//
-// test('movePaneSecondary(direction): no op if adjacent grid cannot import pane', t => {
-//   t.plan(2)
-//   try {
-//     const paneMover = stubPaneMover(9)
-//     const modes = [
-//       {
-//         id: 0,
-//         grid: {id: 0},
-//         movePaneSecondary: sinon.stub.throws(),
-//         canImport: sinon.spy(),
-//         importPane: sinon.spy(),
-//         exportPane: sinon.spy()
-//       },
-//       {
-//         id: 1,
-//         grid: {id: 1},
-//         movePaneSecondary: sinon.spy(),
-//         canImport: () => false,
-//         importPane: sinon.spy(),
-//         exportPane: sinon.spy()
-//       }
-//     ]
-//     const sGrid = {
-//       adjacentGrids: [{left: {id: 1}}]
-//     }
-//     const { movePaneSecondary } = paneMover({modes, sGrid})
-//     movePaneSecondary('left')
-//     t.ok(modes[0].exportPane.notCalled, 'pane not exported from grid')
-//     t.ok(modes[1].importPane.notCalled, 'pane not imported to non-adjacent grid')
-//   } catch (e) {
-//     t.fail(e)
-//   }
-// })
+test('paneOccupiesSameSpaceAtDirection(pane, winBounds, direction): ' +
+     'returns true when appropriate left and up', t => {
+  t.plan(1)
+  try {
+    const { paneOccupiesSameSpaceAtDirection } = require('../../lib/utils')
+    const pane = {x: 900, width: 500, height: 250, y: 0}
+    const winBounds = {x: 300, width: 100, y: 50, height: 100}
+    t.ok(
+      paneOccupiesSameSpaceAtDirection(pane, winBounds, 'left'),
+      'returns true when occupied same directional space'
+    )
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('paneOccupiesSameSpaceAtDirection(pane, winBounds, direction): ' +
+     'returns true when appropriate left and down', t => {
+  t.plan(1)
+  try {
+    const { paneOccupiesSameSpaceAtDirection } = require('../../lib/utils')
+    const pane = {x: 900, width: 500, height: 250, y: 50}
+    const winBounds = {x: 300, width: 100, y: 50, height: 100}
+    t.ok(
+      paneOccupiesSameSpaceAtDirection(pane, winBounds, 'left'),
+      'returns true when occupied same directional space'
+    )
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('paneOccupiesSameSpaceAtDirection(pane, winBounds, direction): ' +
+     'returns true when appropriate right and up', t => {
+  t.plan(1)
+  try {
+    const { paneOccupiesSameSpaceAtDirection } = require('../../lib/utils')
+    const pane = {x: 300, width: 500, height: 250, y: 0}
+    const winBounds = {x: 900, width: 100, y: 50, height: 100}
+    t.ok(
+      paneOccupiesSameSpaceAtDirection(pane, winBounds, 'right'),
+      'returns true when occupied same directional space'
+    )
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('paneOccupiesSameSpaceAtDirection(pane, winBounds, direction): ' +
+     'returns true when appropriate right and down', t => {
+  t.plan(1)
+  try {
+    const { paneOccupiesSameSpaceAtDirection } = require('../../lib/utils')
+    const pane = {x: 900, width: 500, height: 250, y: 50}
+    const winBounds = {x: 300, width: 100, y: 50, height: 100}
+    t.ok(
+      paneOccupiesSameSpaceAtDirection(pane, winBounds, 'right'),
+      'returns true when occupied same directional space'
+    )
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('paneOccupiesSameSpaceAtDirection(pane, winBounds, direction): ' +
+     'returns true when appropriate up and right', t => {
+  t.plan(1)
+  try {
+    const { paneOccupiesSameSpaceAtDirection } = require('../../lib/utils')
+    const pane = {x: 300, width: 300, height: 250, y: 500}
+    const winBounds = {x: 350, width: 400, y: 0, height: 100}
+    t.ok(
+      paneOccupiesSameSpaceAtDirection(pane, winBounds, 'up'),
+      'returns true when occupied same directional space'
+    )
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('paneOccupiesSameSpaceAtDirection(pane, winBounds, direction): ' +
+     'returns true when appropriate up and left', t => {
+  t.plan(1)
+  try {
+    const { paneOccupiesSameSpaceAtDirection } = require('../../lib/utils')
+    const pane = {x: 300, width: 700, height: 250, y: 500}
+    const winBounds = {x: 900, width: 100, y: 50, height: 100}
+    t.ok(
+      paneOccupiesSameSpaceAtDirection(pane, winBounds, 'up'),
+      'returns true when occupied same directional space'
+    )
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('paneOccupiesSameSpaceAtDirection(pane, winBounds, direction): ' +
+     'returns true when appropriate down and right', t => {
+  t.plan(1)
+  try {
+    const { paneOccupiesSameSpaceAtDirection } = require('../../lib/utils')
+    const pane = {x: 350, width: 400, y: 0, height: 100}
+    const winBounds = {x: 300, width: 300, height: 250, y: 500}
+    t.ok(
+      paneOccupiesSameSpaceAtDirection(pane, winBounds, 'down'),
+      'returns true when occupied same directional space'
+    )
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('paneOccupiesSameSpaceAtDirection(pane, winBounds, direction): ' +
+     'returns true when appropriate up and left', t => {
+  t.plan(1)
+  try {
+    const { paneOccupiesSameSpaceAtDirection } = require('../../lib/utils')
+    const pane = {x: 900, width: 100, y: 50, height: 100}
+    const winBounds = {x: 300, width: 700, height: 250, y: 500}
+    t.ok(
+      paneOccupiesSameSpaceAtDirection(pane, winBounds, 'down'),
+      'returns true when occupied same directional space'
+    )
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('paneOccupiesSameSpaceAtDirection(pane, winBounds, direction): ' +
+     'returns false when pane does not occupy same space', t => {
+  t.plan(1)
+  try {
+    const { paneOccupiesSameSpaceAtDirection } = require('../../lib/utils')
+    const pane = {x: 900, width: 100, y: 50, height: 100}
+    const winBounds = {x: 300, width: 100, height: 100, y: 500}
+    t.notOk(
+      paneOccupiesSameSpaceAtDirection(pane, winBounds, 'left'),
+      'returns true when occupied same directional space'
+    )
+  } catch (e) {
+    t.fail(e)
+  }
+})
