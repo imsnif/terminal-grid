e4d219f3ebfae77c2706c76dbbf709010096502d
Aram Drevekenin <grimsniffer@gmail.com>
2016-11-11 15:11:40 +0200

    test: move or throw method

---
+++ test/unit/utils.spec.js
@@ -0,0 +1,299 @@
+const test = require('tape')
+const sinon = require('sinon')
+
+test('moveOrThrow(sGrid, direction, move): moves pane and does not throw if it moved', t => {
+  t.plan(2)
+  try {
+    const { moveOrThrow } = require('../../lib/utils')
+    const currentPanePosition = sinon.stub()
+    currentPanePosition.onCall(0).returns({x: 0, y: 0, width: 100, height: 100})
+    currentPanePosition.onCall(1).returns({x: 10, y: 0, width: 100, height: 100})
+    const sGrid = {currentPanePosition}
+    const move = sinon.spy()
+    moveOrThrow(sGrid, 'right', move)
+    t.ok(currentPanePosition.calledTwice, 'position called twice')
+    t.ok(move.calledOnce, 'move called once')
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('moveOrThrow(sGrid, direction, move): throws if pane not moved', t => {
+  t.plan(3)
+  try {
+    const { moveOrThrow } = require('../../lib/utils')
+    const currentPanePosition = sinon.stub()
+    currentPanePosition.onCall(0).returns({x: 0, y: 0, width: 100, height: 100})
+    currentPanePosition.onCall(1).returns({x: 0, y: 0, width: 100, height: 100})
+    const sGrid = {currentPanePosition}
+    const move = sinon.spy()
+    t.throws(
+      () => moveOrThrow(sGrid, 'right', move),
+      /location blocked/,
+      'method thrown if pane not moved'
+    )
+    t.ok(currentPanePosition.calledTwice, 'position called twice')
+    t.ok(move.calledOnce, 'move called once')
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('moveOrThrow(sGrid, direction, move): moves pane and does not throw if it was resized', t => {
+  t.plan(2)
+  try {
+    const { moveOrThrow } = require('../../lib/utils')
+    const currentPanePosition = sinon.stub()
+    currentPanePosition.onCall(0).returns({x: 0, y: 0, width: 100, height: 100})
+    currentPanePosition.onCall(1).returns({x: 0, y: 0, width: 200, height: 100})
+    const sGrid = {currentPanePosition}
+    const move = sinon.spy()
+    moveOrThrow(sGrid, 'right', move)
+    t.ok(currentPanePosition.calledTwice, 'position called twice')
+    t.ok(move.calledOnce, 'move called once')
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+// test('movePanePrimary(direction): no-op when no focused window', t => {
+//   t.plan(1)
+//   try {
+//     const paneMover = stubPaneMoverNoMode()
+//     const modes = [ {movePaneMain: sinon.spy()}, {movePaneMain: sinon.spy()} ]
+//     const { movePanePrimary } = paneMover({modes})
+//     movePanePrimary('left')
+//     t.ok(modes.every(m => m.movePaneMain.notCalled), 'no movePaneMain method called')
+//   } catch (e) {
+//     t.fail(e)
+//   }
+// })
+//
+// test('movePanePrimary(direction): skips pane to next grid if it exists', t => {
+//   t.plan(1)
+//   try {
+//     const paneMover = stubPaneMover(9)
+//     const modes = [
+//       {
+//         id: 0,
+//         grid: {id: 0},
+//         movePaneMain: sinon.stub.throws(),
+//         canImport: sinon.spy(),
+//         importPane: sinon.spy(),
+//         exportPane: () => 'I am a pane'
+//       },
+//       {
+//         id: 1,
+//         grid: {id: 1},
+//         movePaneMain: sinon.spy(),
+//         canImport: () => true,
+//         importPane: sinon.spy(),
+//         exportPane: sinon.spy()
+//       }
+//     ]
+//     const sGrid = {
+//       adjacentGrids: [{left: {id: 1}}]
+//     }
+//     const { movePanePrimary } = paneMover({modes, sGrid})
+//     movePanePrimary('left')
+//     t.ok(modes[1].importPane.calledWith('I am a pane', 'left'), 'pane properly imported into adjacent grid')
+//   } catch (e) {
+//     t.fail(e)
+//   }
+// })
+//
+// test('movePanePrimary(direction): no op if no room for grid and no adjacent grid', t => {
+//   t.plan(2)
+//   try {
+//     const paneMover = stubPaneMover(9)
+//     const modes = [
+//       {
+//         id: 0,
+//         grid: {id: 0},
+//         movePaneMain: sinon.stub.throws(),
+//         canImport: sinon.spy(),
+//         importPane: sinon.spy(),
+//         exportPane: sinon.spy()
+//       },
+//       {
+//         id: 1,
+//         grid: {id: 1},
+//         movePaneMain: sinon.spy(),
+//         canImport: () => true,
+//         importPane: sinon.spy(),
+//         exportPane: sinon.spy()
+//       }
+//     ]
+//     const sGrid = {
+//       adjacentGrids: [{right: {id: 1}}]
+//     }
+//     const { movePanePrimary } = paneMover({modes, sGrid})
+//     movePanePrimary('left')
+//     t.ok(modes[0].exportPane.notCalled, 'pane not exported from grid')
+//     t.ok(modes[1].importPane.notCalled, 'pane not imported to non-adjacent grid')
+//   } catch (e) {
+//     t.fail(e)
+//   }
+// })
+//
+// test('movePanePrimary(direction): no op if adjacent grid cannot import pane', t => {
+//   t.plan(2)
+//   try {
+//     const paneMover = stubPaneMover(9)
+//     const modes = [
+//       {
+//         id: 0,
+//         grid: {id: 0},
+//         movePaneMain: sinon.stub.throws(),
+//         canImport: sinon.spy(),
+//         importPane: sinon.spy(),
+//         exportPane: sinon.spy()
+//       },
+//       {
+//         id: 1,
+//         grid: {id: 1},
+//         movePaneMain: sinon.spy(),
+//         canImport: () => false,
+//         importPane: sinon.spy(),
+//         exportPane: sinon.spy()
+//       }
+//     ]
+//     const sGrid = {
+//       adjacentGrids: [{left: {id: 1}}]
+//     }
+//     const { movePanePrimary } = paneMover({modes, sGrid})
+//     movePanePrimary('left')
+//     t.ok(modes[0].exportPane.notCalled, 'pane not exported from grid')
+//     t.ok(modes[1].importPane.notCalled, 'pane not imported to non-adjacent grid')
+//   } catch (e) {
+//     t.fail(e)
+//   }
+// })
+//
+// test('movePaneSecondary(direction): moves pane secondary in direction if there is room', t => {
+//   t.plan(2)
+//   try {
+//     const paneMover = stubPaneMover()
+//     const modes = [ {movePaneSecondary: sinon.spy()}, {movePaneSecondary: sinon.spy()} ]
+//     const { movePaneSecondary } = paneMover({modes})
+//     movePaneSecondary('left')
+//     t.ok(modes[0].movePaneSecondary.calledWith('left'), 'movePaneSecondary of current mode called')
+//     t.ok(modes[1].movePaneSecondary.notCalled, 'movePaneSecondary of other mode not called')
+//   } catch (e) {
+//     t.fail(e)
+//   }
+// })
+//
+// test('movePaneSecondary(direction): no-op when no focused window', t => {
+//   t.plan(1)
+//   try {
+//     const paneMover = stubPaneMoverNoMode()
+//     const modes = [ {movePaneSecondary: sinon.spy()}, {movePaneSecondary: sinon.spy()} ]
+//     const { movePaneSecondary } = paneMover({modes})
+//     movePaneSecondary('left')
+//     t.ok(modes.every(m => m.movePaneSecondary.notCalled), 'no movePaneSecondary method called')
+//   } catch (e) {
+//     t.fail(e)
+//   }
+// })
+//
+// test('movePaneSecondary(direction): skips pane to next grid if it exists', t => {
+//   t.plan(1)
+//   try {
+//     const paneMover = stubPaneMover(9)
+//     const modes = [
+//       {
+//         id: 0,
+//         grid: {id: 0},
+//         movePaneSecondary: sinon.stub.throws(),
+//         canImport: sinon.spy(),
+//         importPane: sinon.spy(),
+//         exportPane: () => 'I am a pane'
+//       },
+//       {
+//         id: 1,
+//         grid: {id: 1},
+//         movePaneSecondary: sinon.spy(),
+//         canImport: () => true,
+//         importPane: sinon.spy(),
+//         exportPane: sinon.spy()
+//       }
+//     ]
+//     const sGrid = {
+//       adjacentGrids: [{left: {id: 1}}]
+//     }
+//     const { movePaneSecondary } = paneMover({modes, sGrid})
+//     movePaneSecondary('left')
+//     t.ok(modes[1].importPane.calledWith('I am a pane', 'left'), 'pane properly imported into adjacent grid')
+//   } catch (e) {
+//     t.fail(e)
+//   }
+// })
+//
+// test('movePaneSecondary(direction): no op if no room for grid and no adjacent grid', t => {
+//   t.plan(2)
+//   try {
+//     const paneMover = stubPaneMover(9)
+//     const modes = [
+//       {
+//         id: 0,
+//         grid: {id: 0},
+//         movePaneSecondary: sinon.stub.throws(),
+//         canImport: sinon.spy(),
+//         importPane: sinon.spy(),
+//         exportPane: sinon.spy()
+//       },
+//       {
+//         id: 1,
+//         grid: {id: 1},
+//         movePaneSecondary: sinon.spy(),
+//         canImport: () => true,
+//         importPane: sinon.spy(),
+//         exportPane: sinon.spy()
+//       }
+//     ]
+//     const sGrid = {
+//       adjacentGrids: [{right: {id: 1}}]
+//     }
+//     const { movePaneSecondary } = paneMover({modes, sGrid})
+//     movePaneSecondary('left')
+//     t.ok(modes[0].exportPane.notCalled, 'pane not exported from grid')
+//     t.ok(modes[1].importPane.notCalled, 'pane not imported to non-adjacent grid')
+//   } catch (e) {
+//     t.fail(e)
+//   }
+// })
+//
+// test('movePaneSecondary(direction): no op if adjacent grid cannot import pane', t => {
+//   t.plan(2)
+//   try {
+//     const paneMover = stubPaneMover(9)
+//     const modes = [
+//       {
+//         id: 0,
+//         grid: {id: 0},
+//         movePaneSecondary: sinon.stub.throws(),
+//         canImport: sinon.spy(),
+//         importPane: sinon.spy(),
+//         exportPane: sinon.spy()
+//       },
+//       {
+//         id: 1,
+//         grid: {id: 1},
+//         movePaneSecondary: sinon.spy(),
+//         canImport: () => false,
+//         importPane: sinon.spy(),
+//         exportPane: sinon.spy()
+//       }
+//     ]
+//     const sGrid = {
+//       adjacentGrids: [{left: {id: 1}}]
+//     }
+//     const { movePaneSecondary } = paneMover({modes, sGrid})
+//     movePaneSecondary('left')
+//     t.ok(modes[0].exportPane.notCalled, 'pane not exported from grid')
+//     t.ok(modes[1].importPane.notCalled, 'pane not imported to non-adjacent grid')
+//   } catch (e) {
+//     t.fail(e)
+//   }
+// })
