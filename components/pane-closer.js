fd180c70c7a17fcd2fb016bfc09e9f507e161802
Aram Drevekenin <grimsniffer@gmail.com>
2016-11-20 22:03:18 +0200

    feat: focus on adjacent pane when pane is closed

--- components/pane-closer.js
+++ components/pane-closer.js
@@ -9,8 +9,16 @@ module.exports = function paneMover (state, closeAction) {
       if (!focusedWindow) return
       const pane = state.grid.panes.filter(p => p.id === focusedWindow.id)[0]
       if (!pane) return // not part of this grid
+      const adjacentPanesInGrid = ['right', 'left', 'up', 'down']
+        .map(d => state.sGrid.adjacentPaneInGrid(d))
+        .filter(p => p)
+      const adjacentPanesOutsideGrid = ['right', 'left', 'up', 'down']
+        .map(d => state.sGrid.adjacentPane(d))
+        .filter(p => p)
       focusedWindow.close()
       if (closeAction) closeAction()
+      if (adjacentPanesInGrid.length > 0) return adjacentPanesInGrid[0].wrapped.focus()
+      if (adjacentPanesOutsideGrid.length > 0) return adjacentPanesOutsideGrid[0].wrapped.focus()
     }
   }
 }
