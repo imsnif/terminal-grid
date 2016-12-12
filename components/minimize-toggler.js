8199848df7f561b5628f74d73c6c11b5e208c76e
Aram Drevekenin <grimsniffer@gmail.com>
2016-12-13 00:05:45 +0200

    feat(#30): focus pane on max/min all panes

--- components/minimize-toggler.js
+++ components/minimize-toggler.js
@@ -1,12 +1,20 @@
 'use strict'
 
+const { BrowserWindow } = require('electron')
+
 module.exports = function minimizeToggler (state) {
   return {
     togglePaneFullSize: () => {
       state.sGrid.toggleCurrentWinFullSize() // TODO: move this function here
     },
     toggleAllShow: () => {
+      const currentFocused = BrowserWindow.getFocusedWindow()
       state.wChanger.toggleAllShow()
+      if (currentFocused) {
+        state.lastFocusedWindow = currentFocused
+      } else if (state.lastFocusedWindow) {
+        state.lastFocusedWindow.focus()
+      }
     }
   }
 }
