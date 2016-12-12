99200a0e1623681d53f6d44bdc1cd330b93f3363
Aram Drevekenin <grimsniffer@gmail.com>
2016-12-13 00:05:17 +0200

    test(#30): focus window when minimizing/maximizing all panes

--- test/unit/minimize-toggler.spec.js
+++ test/unit/minimize-toggler.spec.js
@@ -1,13 +1,18 @@
 const test = require('tape')
 const sinon = require('sinon')
+const proxyquire = require('proxyquire').noCallThru()
 
-const minimizeToggler = require('../../components/minimize-toggler')
+function stubMinimizeToggler (focusedPane) {
+  return proxyquire('../../components/minimize-toggler', {
+    'electron': {BrowserWindow: {getFocusedWindow: () => focusedPane}}
+  })
+}
 
 test('togglePanefullSize(): toggles pane full size', t => {
   t.plan(1)
   try {
     const sGrid = {toggleCurrentWinFullSize: sinon.spy()}
-    const { togglePaneFullSize } = minimizeToggler({sGrid})
+    const { togglePaneFullSize } = stubMinimizeToggler()({sGrid})
     togglePaneFullSize()
     t.ok(sGrid.toggleCurrentWinFullSize.calledOnce, 'toggleCurrentWinFullSize called')
   } catch (e) {
@@ -15,13 +20,29 @@ test('togglePanefullSize(): toggles pane full size', t => {
   }
 })
 
-test('toggleAllShow(): toggles all show', t => {
-  t.plan(1)
+test('toggleAllShow(): toggles all show when windows are minimized', t => {
+  t.plan(2)
   try {
     const wChanger = {toggleAllShow: sinon.spy()}
-    const { toggleAllShow } = minimizeToggler({wChanger})
+    const focus = sinon.spy()
+    const { toggleAllShow } = stubMinimizeToggler()({wChanger, lastFocusedWindow: {focus}})
     toggleAllShow()
     t.ok(wChanger.toggleAllShow.calledOnce, 'toggleAllShow called')
+    t.ok(focus.calledOnce, 'last focused window focused again')
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('toggleAllShow(): toggles all show when windows are restored', t => {
+  t.plan(2)
+  try {
+    const wChanger = {toggleAllShow: sinon.spy()}
+    let state = {wChanger}
+    const { toggleAllShow } = stubMinimizeToggler({id: 1})(state)
+    toggleAllShow()
+    t.ok(wChanger.toggleAllShow.calledOnce, 'toggleAllShow called')
+    t.deepEquals(state.lastFocusedWindow, {id: 1}, 'last focused window set on state')
   } catch (e) {
     t.fail(e)
   }
